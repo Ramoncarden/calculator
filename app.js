5cf587adf4026ad8763228c97c3786352c92ccd7
Ramon Cardenas <ramoncarden@gmail.com>
2021-11-18 01:02:27 -0600

    Added mathematical operations functionlity.

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ let operation = {
   firstInput: '',
   secondInput: '',
   operandPressed: false,
+  action: '',
+  runningTotal: undefined,
 };
 
 const evaluate = (e) => {
@@ -20,17 +22,55 @@ const evaluate = (e) => {
 };
 
 const getMathOperation = (e) => {
-  if (operation.firstInput.length >= 1) {
+  if (operation.firstInput.length >= 1 && operation.secondInput === '') {
     operation.operandPressed = true;
+    operation.action = e.target.innerText;
     output.innerText = e.target.innerText;
     console.log(e.target.innerText);
   }
 };
 
-let inputElements = document.querySelectorAll('.input-btn').forEach((item) => {
-  item.addEventListener('click', evaluate);
-});
-// document.addEventListener('click', evaluate);
+const checkInt = (func) => {
+  if (Number.isInteger()) {
+    output.innerText = func();
+  } else {
+    output.innerText = Math.round((func + Number.EPSILON) * 100) / 100;
+  }
+};
+
+function getTotal() {
+  switch (true) {
+    case operation.action === '+':
+      const sumNums = () =>
+        Number(operation.firstInput) + Number(operation.secondInput);
+      // if (Number.isInteger(sumNums())) {
+      //   output.innerText = sumNums();
+      // } else {
+      //   output.innerText = Math.round((sumNums() + Number.EPSILON) * 100) / 100;
+      // }
+      checkInt(sumNums());
+      break;
+    case operation.action === '-':
+      const subNums = () =>
+        Number(operation.firstInput) - Number(operation.secondInput);
+      checkInt(subNums());
+      break;
+    case operation.action === 'x':
+      const multNums = () =>
+        Number(operation.firstInput) * Number(operation.secondInput);
+      checkInt(multNums());
+      break;
+    case operation.action === '÷':
+      const divNums = () =>
+        Number(operation.firstInput) / Number(operation.secondInput);
+      checkInt(divNums());
+      break;
+    default:
+      output.innerText = 0;
+  }
+  operation.runningTotal = output.innerText;
+  console.log(operation.runningTotal);
+}
 
 document.getElementById('clear').addEventListener('click', () => {
   operation.firstInput = '';
@@ -39,8 +79,15 @@ document.getElementById('clear').addEventListener('click', () => {
   output.innerText = '0';
 });
 
-let operatorClick = document.querySelectorAll('.operator').forEach((item) => {
+// **************** DOM selectors *******************
+
+let inputElements = document.querySelectorAll('.input-btn').forEach((item) => {
+  item.addEventListener('click', evaluate);
+});
+
+let operatorClick = document.querySelectorAll('.operand').forEach((item) => {
   item.addEventListener('click', getMathOperation);
 });
 
-// TODO: Capture second variable info
+const equal = document.getElementById('equals');
+equal.addEventListener('click', getTotal);
